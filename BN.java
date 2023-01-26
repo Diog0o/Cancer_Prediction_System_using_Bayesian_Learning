f0b6b9df134e946c844af63389654528bd392fad
Diog0o <75538603+Diog0o@users.noreply.github.com>
2023-01-26 16:15:48 +0000

    Update BN.java

--- BN.java
+++ BN.java
@@ -15,57 +15,7 @@ public class BN implements Serializable{
 	ArrayList<double[][]> DFO;
 	
 	
-	/*
-	 * public BN (int[] arvore, Amostra amostra, double S){
-	 * 
-	 * arvore = [3,0,1,5]
-	 * recebe uma lista (arvore) e olha para cada posicao (filho) e tira o pai (entrada)
-	 * 
-	 * 
-	 * para cada par de pai/filho, calcula matriz de prob condicionada domain cenas
-	 * quando nao tem pais, fazemos so uma linha/coluna so com as proprias contagens
-	 * 
-	 * tem que devolver o tuplo arvore e teta (matrizes)
-	 * prob condic e aquela continha (slide 13)
-	 * 
-	 * }
-	 */
-	
-	/* public BN (int[] arvore, Amostra amostra, double S){
-		this.arvore= arvore;
-		this.DFO = new ArrayList<double[][]>();
-		for (int i = 0; i < arvore.length ; i++) { // i percorre os nos
-			int pai = arvore[i];
-			int[] Fz = {i}; //Fz e a variavel (cor)
-			int[] Pz = {pai}; //Pz e a variavel (tamanho)
-			// i e o filho
-			
-			if (pai==-1) { //se encontrarmos a raiz
-				double[][] matriz = bob_mbo(i,amostra);
-				for (int k = 0; k<amostra.domain(i); k++) {
-					int[] K = {k}; //ks sao os valores da variavel (branco, castanho, azul...)
-					matriz[k][0] = (amostra.count(Fz, K) + S) / (amostra.length() + S* amostra.domain(i)) ;
-				}
-				this.DFO.add(matriz);
-			}
-			
-			else {
-				double[][] matriz = bob_mb(i, pai, amostra);
-				for (int k = 0; k < amostra.domain(i); k++) {
-					for (int j = 0; j < amostra.domain(pai) ; j++) {
-						int[] var = {i, pai};
-						int[] val = {k,j};
-						//int[] K= {k};
-						int[] J = {j};
-						matriz[k][j] = (amostra.count(var, val) + S) / (amostra.count(Pz,J) + S* amostra.domain(i)) ;
-					}
-				}
-				this.DFO.add(matriz);
-			}
-		}
-	}
-	*/
-	
+
 	public double[][] bob_mb(int f, int p, Amostra amostra){
 		//double[][] r = new double[amostra.domain(f)][amostra.domain(p)];
 		return new double[amostra.domain(f)][amostra.domain(p)];
@@ -115,17 +65,7 @@ public class BN implements Serializable{
 		return this.arvore;
 	}
 	
-	/*public double continha(Amostra amostra, double S, int Pz, int p, int Fz, int f) {
-		int[] var = {Pz,Fz};
-		int[] val = {p,f};
-		int[] Pai = {Pz};
-		int[] pai = {p};
-		int[] Filho = {Fz};
-		return (amostra.count(var,val)+S)/(amostra.count(Pai,pai) + S*amostra.domain(Filho)); 
-	}
-	*/
 	
-	//PERGUNTA o que e um vetor? (no, valor_no, valor_pai)?
 	
 	public double prob (int[] vector) { 
 		double res = 1;
